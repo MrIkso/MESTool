c19488a74c605e36560c6a77d74a5fa38091d1d3
MrIkso <mrikso821@gmail.com>
2025-06-11 01:56:06 +0300

    Removed useless code

--- MESTool/MainForm.cs
+++ MESTool/MainForm.cs
@@ -1,16 +1,7 @@
-﻿
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
+﻿using System.ComponentModel;
 using System.Diagnostics;
-using System.Drawing;
-using System.Drawing.Imaging;
-using System.IO;
-using System.Linq;
 using System.Reflection;
-using System.Runtime.InteropServices;
 using System.Text;
-using System.Windows.Forms;
 using static MESTool.MesToolProcessor;
 
 namespace MESTool
